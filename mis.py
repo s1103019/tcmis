ba2f086694cb5c37dc4006a846b3e31c9ad6a40f
杜鎧斈 <s1103019@gm.pu.edu.tw>
2022-10-11 11:31:02 +0800

    make it better

---
+++ mis.py
@@ -0,0 +1,5 @@
+def mis2a():
+    print("學號:411030193")
+
+def mis2b():
+    print("姓名:杜鎧斈")
---
+++ use1.py
@@ -0,0 +1,10 @@
+import mis
+
+x = int(input("查詢姓名輸入1, 學號輸入2"))
+
+if(x==1):
+	mis.mis2a()
+elif(x==2):
+	mis.mis2b()
+else:
+	print("無法查詢")
